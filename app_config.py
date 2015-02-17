6feaa439a0ba3bbdab9b352289f3e574d9ec47a6
chagan <chrishagan23@gmail.com>
2015-02-17 12:08:53 -0600

    Removed NPR favicon

--- app_config.py
+++ app_config.py
@@ -22,7 +22,7 @@ PROJECT_FILENAME = '$NEW_PROJECT_FILENAME'
 
 # The name of the repository containing the source
 REPOSITORY_NAME = '$NEW_REPOSITORY_NAME'
-GITHUB_USERNAME = ‘lojaff’
+GITHUB_USERNAME = 'wbez'
 REPOSITORY_URL = 'git@github.com:%s/%s.git' % (GITHUB_USERNAME, REPOSITORY_NAME)
 REPOSITORY_ALT_URL = None # 'git@bitbucket.org:nprapps/%s.git' % REPOSITORY_NAME'
 
